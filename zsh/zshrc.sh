42c06e9d1d3a5def62e8bfb2bbc40edd71aaa8e8
Jeff Yoon <jeyoon@outlook.com>
2017-10-23 20:14:28 +0900

    Update zshrc for my system

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,3 +1,14 @@
+# Path to your oh-my-zsh installation.
+export ZSH=/Users/jeffyoon/dotfiles/zsh/plugins/oh-my-zsh
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git node npm osx)
+
+source $ZSH/oh-my-zsh.sh
+
 # Vars
 	HISTFILE=~/.zsh_history
 	SAVEHIST=1000 
